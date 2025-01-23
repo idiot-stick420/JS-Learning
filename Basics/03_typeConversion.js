d46bfc897d4e0299c1155ca2b335bbb148648be7
idiot-stick420 <bajajujwal1234@gmail.com>
2025-01-23 16:57:24 +0000

    Conversion of Data Types

---
+++ Basics/03_typeConversion.js
@@ -0,0 +1,13 @@
+let score = null;
+
+// console.log(typeof(score));
+// let value = Number(score); // Can convert anythong, but if something wrong is passed, will give type number nd value NaN
+
+// console.log(typeof(value));
+// console.log(value);
+
+// score = null => value = 0
+// score = undefined => value = NaN
+
+let bool = Boolean(score); // undefined => false!!
+console.log(bool);
